99f7f9e078c9900ba1e9a74a8ddbedc11c7a7577
brian <bthomas@tsl.texas.gov>
2021-02-02 11:07:30 -0600

    example script

---
+++ basicExample.py
@@ -0,0 +1,21 @@
+import opexCreator
+import getpass
+username = input("username: ")
+prefix = input("prefix: ")
+tenancy = input("tenancy: ")
+password = getpass.getpass("Enter password: ")
+valuables = {'username': username,
+             'password': password,
+             'tenent': tenancy,
+             'prefix': prefix,
+             'access_directory': '/media/sf_G_DRIVE/working_electronicRecords/research_3a.001b/opex/video/presentation',
+             'preservation_directory': '/media/sf_G_DRIVE/working_electronicRecords/research_3a.001b/opex/video/preservation',
+             'asset_title': 'captionTest',
+             'asset_tag': 'open',
+             'parent_uuid': 'cf771ef6-ccc0-4102-9576-5713decdc35e',
+             'export_directory': '/media/sf_G_DRIVE/working_electronicRecords/research_3a.001b/opex/test',
+             'asset_description': 'captionTest',
+             'ignore': [".metadata", ".db"],
+             'special_format': 'film'}
+
+opexCreator.multi_upload_withXIP(valuables)
